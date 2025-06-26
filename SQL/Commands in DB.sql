403ed95b3cb3b518ae23887fc63d20f4b6c922ab
Ved <askrocks2001@gmail.com>
2025-06-26 11:53:48 +0530

    Types of Command in SQL

---
+++ SQL/Commands in DB.sql
@@ -0,0 +1,6 @@
+-- there are 5 types of sql commands :
+# 1: DDL : data definition language: ( create , alter, rename, truncate, and drop )
+# 2: DML : data manipulation language: ( insert , update and delete )
+# 3:  DQL : data query language: ( select )
+# 4: DCL : data control language: ( grant and revoke , used to grant the users )
+#5: TCL : transaction control language: ( start, commit, rollback )
