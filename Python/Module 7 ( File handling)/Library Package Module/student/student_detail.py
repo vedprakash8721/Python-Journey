23e2739299bc2ba27f09dc64a1b86a0f76704b92
Ved <askrocks2001@gmail.com>
2025-06-30 15:34:16 +0530

    creating module and performing import operation

---
+++ Python/Module 7 ( File handling)/Library Package Module/student/student_detail.py
@@ -0,0 +1,11 @@
+# from teacher import teacher_detail
+import os # os provides funtionality to interact with operating system
+import sys # provide accesss to system specific parameter and function such as python path
+from os.path import dirname, join, abspath
+#__file__ gives you the path of current script
+# it gives you:C:\MY SPACE\Data Science and Gen AI\Abhishek-Data-Science-Portfolio\Python\Module 7 ( File handling)\Library Package Module\Student\student_detail.py """
+print("the path of this file is:",__file__)
+def student():
+    print("this is student  module:")
+
+# teacher_detail.teacher()
---
+++ Python/Module 7 ( File handling)/Library Package Module/teacher/teacher_detail.py
@@ -0,0 +1,3 @@
+def teacher():
+    print("this is teacher module:")
+teacher()
